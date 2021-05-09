d8813a22f5a57c90535e4af21c35fd77935a2d73
tootal <tootal@yeah.net>
2021-05-09 17:25:11 +0800

    init project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world!</h1>');
+});
+
+server.listen(3000, () => {
+    console.log('listening on port 3000');
+});
+
